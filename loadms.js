c2e7f8e6a533a219c0303454580b869c4b10b1bf
Turgut Harun Arslan <83730616+TurgutHarunArslan@users.noreply.github.com>
2022-07-07 23:44:54 +0300

    5.0

--- loadms.js
+++ loadms.js
@@ -1,99 +1,49 @@
 import {database} from "./firebase.js";
-import {ref, set, get, child, getDatabase } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-database.js";
+import {getDatabase, ref, onValue, child} from "https://www.gstatic.com/firebasejs/9.8.4/firebase-database.js";
 
 
 
-/* window.onload = function(){
-    const starCountRef = ref(database, 'messages/' );
-    get(starCountRef, (snapshot) => {
-      let data = snapshot.val();
+  let databasem = getDatabase();
 
-      for(let i = 0; i <  Object.values(data).length; i++){
-        let valz =  Object.values(data)[i];
-        Object.values(valz)[0];
 
-        if(Object.values(valz)[1] == 'nazan'){
-         var div = document.createElement('div');
-         var p = document.createElement('p');
-         var img = document.createElement('img');
-             div.classList.add('tweet2');
-             div.classList.add('tweet');
-                div.style = 'position: releative; display:flex; width: 500px; height: fit-content; margin-top:40px; margin-right:20px;';
-                img.style = 'width:60px; height: 60px;';
-                p.style = 'margin-left:10px; margin-top: 20px;  background-color: rgba(4, 195, 253, 0.801) !important; font-weight: bold; border-radius: 20px; text-align: center;';
-                 img.src = 'prof2.webp';
-                 p.textContent =   Object.values(valz)[0];
-                document.getElementsByTagName('body')[0].appendChild(div);
-                div.appendChild(p);
-                div.appendChild(img);
-                
-        }else if(Object.values(valz)[1] == 'turgut'){
-          var div = document.createElement('div');
-          var p = document.createElement('p');
-          var img = document.createElement('img');
-              div.classList.add('tweet');
-          
-                 div.style = 'position: releative; display:flex; width: 500px; height: fit-content; margin-top:40px; margin-left:20px;';
-                 img.style = 'width:60px; height: 60px;';
-                 p.style = 'margin-left:10px; margin-top: 20px;  background-color: rgb(15, 235, 15) !important; font-weight: bold; border-radius: 20px; text-align: center;';
-                  img.src = 'prof.png';
-        
-                  p.textContent =   Object.values(valz)[0];
-                 document.getElementsByTagName('body')[0].appendChild(div);
-                 div.appendChild(img);
-                 div.appendChild(p);
-               }
-       };
-   
-      });
-    };
-    */
-    window.onload = function(){
-    const dbRef = ref(getDatabase());
-    get(child(dbRef, '/messages')).then((snapshot) => {
-      if (snapshot.exists()) {
-        console.log(snapshot.val());
-        const data = snapshot.val();
-        for(let i = 0; i <  Object.values(data).length; i++){
-            let valz =  Object.values(data)[i];
-            Object.values(valz)[0];
-    
-            if(Object.values(valz)[2] == 'nazan'){
-             var div = document.createElement('div');
-             var p = document.createElement('p');
-             var img = document.createElement('img');
-                 div.classList.add('tweet2');
-                 div.classList.add('tweet');
-                    div.style = 'position: releative; display:flex; width: 500px; height: fit-content; margin-top:40px; margin-right:20px;';
-                    img.style = 'width:60px; height: 60px;';
-                    p.style = 'margin-left:10px; margin-top: 20px;  background-color: rgba(4, 195, 253, 0.801) !important; font-weight: bold; border-radius: 20px; text-align: center;';
-                     img.src = 'prof2.webp';
-                     p.textContent =   Object.values(valz)[0];
-                    document.getElementsByTagName('body')[0].appendChild(div);
-                    div.appendChild(p);
-                    div.appendChild(img);
-                    
-            }else if(Object.values(valz)[2] == 'turgut'){
-              var div = document.createElement('div');
-              var p = document.createElement('p');
-              var img = document.createElement('img');
-                  div.classList.add('tweet');
+  const listener =  onValue(ref(databasem, 'messages'), (snapshot) => {
+  const datas = snapshot.val();
+  for(let i = 0; i <  Object.values(datas).length; i++){
+      let valz =  Object.values(datas)[i];
+      Object.values(valz)[0];
+
+      if(Object.values(valz)[2] == 'nazan'){
+
+      
+      var div = document.createElement('div');
+      var span = document.createElement('span');
+      var img = document.createElement('img');
+      var p = document.createElement('p');
+      div.classList.add('container');
+        img.src = 'prof.png';
+        span.textContent = Object.values(valz)[2];
+        p.textContent = Object.values(valz)[0];
+       document.getElementsByTagName('body')[0].appendChild(div);
+       div.appendChild(span);
+       div.appendChild(img);
+       div.appendChild(p);
               
-                     div.style = 'position: releative; display:flex; width: 500px; height: fit-content; margin-top:40px; margin-left:20px;';
-                     img.style = 'width:60px; height: 60px;';
-                     p.style = 'margin-left:10px; margin-top: 20px;  background-color: rgb(15, 235, 15) !important; font-weight: bold; border-radius: 20px; text-align: center;';
-                      img.src = 'prof.png';
-            
-                      p.textContent =   Object.values(valz)[0];
-                     document.getElementsByTagName('body')[0].appendChild(div);
-                     div.appendChild(img);
-                     div.appendChild(p);
-                   }
-           };
-      } else {
-        console.log("No data available");
-      }
-    }).catch((error) => {
-      console.error(error);
-    });
-  };
+      }else if(Object.values(valz)[2] == 'turgut'){
+        var div = document.createElement('div');
+        var span = document.createElement('span');
+        var img = document.createElement('img');
+        var p = document.createElement('p');
+        div.classList.add('container');
+          img.src = 'prof.png';
+          span.textContent = Object.values(valz)[2];
+          p.textContent = Object.values(valz)[0];
+         document.getElementsByTagName('body')[0].appendChild(div);
+         div.appendChild(span);
+         div.appendChild(img);
+         div.appendChild(p);
+
+} {
+setTimeout(() => {
+  listener();
+}, 500);
+}}}); 
--- main.js
+++ main.js
@@ -5,56 +5,52 @@ const db = getDatabase();
 const txt = document.getElementById('tweet');
 const btn = document.getElementById('TweetButton');
 let name = prompt("What is your name?").toLocaleLowerCase();
+let change = document.getElementById('ColorChange');
+
+
+   
+change.onclick = function() {
+  var color = 'rgb(' + 
+  (Math.floor(Math.random()*56)+200) + ', ' +
+  (Math.floor(Math.random()*56)+200) + ', ' +
+  (Math.floor(Math.random()*56)+200) +
+  ')';
+  return color;
+}
+
+
 
 if(name==='turgut' || name === 'nazan'){
 }else{
 window.close()
 }
 
-btn.onclick = function(){
-  const d = new Date();
+
+btn.onclick = function(color){
+const d = new Date();
 let time = d.getTime();
-if(name == 'turgut'){
-let txtv = txt.value;
-var message = txt.value;
+if(name == 'turgut' || name == 'nazan'){
+  let txtv = txt.value;
+  var message = txt.value;
 
 var div = document.createElement('div');
-var p = document.createElement('p');
+var span = document.createElement('span');
 var img = document.createElement('img');
-    div.classList.add('tweet');
-
-       div.style = 'position: releative; display:flex; width: 500px; height: fit-content; margin-top:40px; margin-left:20px;';
-       img.style = 'width:60px; height: 60px;';
-       p.style = 'margin-left:10px; margin-top: 20px;  background-color: rgb(15, 235, 15) !important; font-weight: bold; border-radius: 20px; text-align: center;';
-        img.src = 'prof.png';
-        p.textContent = txtv;
-       document.getElementsByTagName('body')[0].appendChild(div);
-       div.appendChild(img);
-       div.appendChild(p);
-       div.classList.add('mess');
-      }else if(name == 'nazan'){
-        let txtv = txt.value;
-        var message = txt.value;
-
-    var div = document.createElement('div');
 var p = document.createElement('p');
-var img = document.createElement('img');
-    div.classList.add('tweet2');
-    div.classList.add('tweet');
-       div.style = 'position: releative; display:flex; width: 500px; height: fit-content; margin-top:40px; margin-right:20px;';
-       img.style = 'width:60px; height: 60px;';
-       p.style = 'margin-left:10px; margin-top: 20px;  background-color: rgba(4, 195, 253, 0.801) !important; font-weight: bold; border-radius: 20px; text-align: center;';
-        img.src = 'prof2.webp';
-        p.textContent = txtv;
-       document.getElementsByTagName('body')[0].appendChild(div);
-       div.appendChild(p);
-       div.appendChild(img);
+div.classList.add('container');
+  img.src = 'prof.png';
+  span.textContent = name;
+  p.textContent = txtv;
+ document.getElementsByTagName('body')[0].appendChild(div);
+ div.appendChild(span);
+ div.appendChild(img);
+ div.appendChild(p);
       }
  writeUserData(name, message, time);
       
     }
     function writeUserData(name, message, time) {
-      set(ref(database, 'messages/' + time), {
+      set(ref(database, 'messages/' + time + ' ' + name), {
         username: name,
         messages: message,
         time: time
@@ -88,35 +84,32 @@ var img = document.createElement('img');
     let dataz =   Object.values(data)[0];
 
     if(Object.values(dataz)[2] == 'nazan' && name !== 'nazan'){
-     var div = document.createElement('div');
-     var p = document.createElement('p');
-     var img = document.createElement('img');
-         div.classList.add('tweet2');
-         div.classList.add('tweet');
-            div.style = 'position: releative; display:flex; width: 500px; height: fit-content; margin-top:40px; margin-right:20px;';
-            img.style = 'width:60px; height: 60px;';
-            p.style = 'margin-left:10px; margin-top: 20px;  background-color: rgba(4, 195, 253, 0.801) !important; font-weight: bold; border-radius: 20px; text-align: center;';
-             img.src = 'prof2.webp';
-             p.textContent =   Object.values(dataz)[0];
-            document.getElementsByTagName('body')[0].appendChild(div);
-            div.appendChild(p);
-            div.appendChild(img);
+
+  var div = document.createElement('div');
+var span = document.createElement('span');
+var img = document.createElement('img');
+var p = document.createElement('p');
+  div.classList.add('container');
+      img.src = 'prof.png';
+      span.textContent = Object.values(dataz)[2];
+      p.textContent = Object.values(dataz)[0];
+     document.getElementsByTagName('body')[0].appendChild(div);
+     div.appendChild(span);
+     div.appendChild(img);
+     div.appendChild(p);
             
-}else if(Object.values(dataz)[2] == 'turgut' && name !== 'turgut'){
-      var div = document.createElement('div');
-      var p = document.createElement('p');
-      var img = document.createElement('img');
-          div.classList.add('tweet');
-      
-             div.style = 'position: releative; display:flex; width: 500px; height: fit-content; margin-top:40px; margin-left:20px;';
-             img.style = 'width:60px; height: 60px;';
-             p.style = 'margin-left:10px; margin-top: 20px;  background-color: rgb(15, 235, 15) !important; font-weight: bold; border-radius: 20px; text-align: center;';
-              img.src = 'prof.png';
-    
-              p.textContent =   Object.values(dataz)[0];
-             document.getElementsByTagName('body')[0].appendChild(div);
-             div.appendChild(img);
-             div.appendChild(p);
+}else if(Object.values(dataz)[2] !== 'turgut' && name !== 'turgut'){
+  var div = document.createElement('div');
+var span = document.createElement('span');
+var img = document.createElement('img');
+var p = document.createElement('p');
+  div.classList.add('container');
+      img.src = 'prof.png';
+      span.textContent = Object.values(dataz)[2];
+      p.textContent = Object.values(dataz)[0];
+     document.getElementsByTagName('body')[0].appendChild(div);
+     div.appendChild(span);
+     div.appendChild(img);
+     div.appendChild(p);
            } 
    });
-   
