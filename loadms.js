4e5eddcd605a0d8f086f913dc8485f42c88e5225
Turgut Harun Arslan <83730616+TurgutHarunArslan@users.noreply.github.com>
2022-07-09 19:31:52 +0300

    Update loadms.js

--- loadms.js
+++ loadms.js
@@ -11,7 +11,6 @@ import {getDatabase, ref, onValue, child} from "https://www.gstatic.com/firebase
   for(let i = 0; i <  Object.values(datas).length; i++){
       let valz =  Object.values(datas)[i];
       Object.values(valz)[0];
-console.log(Object.values(valz)[3]);
 if (Object.values(valz)[3] == 'undefined') {
   var div = document.createElement('div');
   var span = document.createElement('span');
